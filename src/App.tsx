2a9926c5cbd2d6a788ce217a0007ad7e5d5d01db
ilia_kravtsov <kravtsov.i.s@mail.ru>
2022-12-16 19:41:34 +0300

    React_Samurai_Way_02

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>01</li>
+        <li>02</li>
+        <li>03</li>
+        <li>04</li>
+      </ul>
     </div>
   );
 }
