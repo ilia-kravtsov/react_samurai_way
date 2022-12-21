7e3e030aa53470b462c6250774339fec6e59dea4
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2022-12-21 14:27:07 +0400

    React_Samurai_Way_11

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
 
 const App = () => {
     return (
-        <div className="App">
-            <Header/>
-            <Technologies/>
+        <div className='app_wrapper'>
+            <header className='header'>
+                <img src="https://pluspng.com/img-png/avengers-logo-png-transparent-aou-logo-png-844.png" alt="Groot"/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a href='#'>Profile</a>
+                </div>
+                <div>
+                    <a href='#'>Messages</a>
+                </div>
+                <div>
+                    <a href='#'>News</a>
+                </div>
+                <div>
+                    <a href='#'>Music</a>
+                </div>
+                <div>
+                    <a href='#'>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <img src="https://avatars.mds.yandex.net/i?id=a69847b56ccbe331769d0552889e756a-5234578-images-thumbs&n=13" alt="Groot"/>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My Posts
+                    <div>
+                        New Post
+                    </div>
+                    <div>
+                        <div>
+                            Post_1
+                        </div>
+                        <div>
+                            Post_2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
